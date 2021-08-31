46cd1e40c6a97069fbfe69d497deb524a05dddbd
jasmeen7202 <jasmeen7202@gmail.com>
2021-09-01 01:04:16 +0530

    displayed game history

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,42 +1,61 @@
-import React, { useState }from "react";
+import React, { useState } from 'react';
 import Board from './components/Board';
-import { calculateWinner } from "./helpers";
+import History from './components/History';
+import { calculateWinner } from './helpers';
 
 import './styles/root.scss';
 
 const App = () => {
-  const [board, setBoard] = useState( Array(9).fill(null));
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
 
-    // console.log(board);
+  // console.log(board);
+  // const [isXNext, setIsXNext] = useState(false);
 
-    const [isXNext, setIsXNext] = useState(false);
+  const [currentMove, setCurrentMove] = useState(0);
 
-    const winner = calculateWinner(board);
-    
-    const message = winner ? `Winner is ${winner}` : `Next player is ${isXNext ? 'X' : 'O'}`;
-    const handleSquareClick = (position) => {
-        // setBoard(5);
+  const current = history[currentMove];
 
-        if( board[position] || winner){ // if the board has some position, then it will not be changed
-          // if there is a winner, then we can't play further.
-            return;
+  console.log('history', history);
+
+  const winner = calculateWinner(current.board);
+
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
+  const handleSquareClick = position => {
+    // setBoard(5);
+
+    if (current.board[position] || winner) {
+      // if the board has some position, then it will not be changed
+      // if there is a winner, then we can't play further.
+      return;
+    }
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? 'X' : 'O';
         }
-        setBoard( (prev) => {
-            return prev.map((square, pos) => {
-                if(pos === position) {
-                    return isXNext ? 'X' : 'O';
-                }
-                return square;
-            })
-        });
-        setIsXNext( (prev) => !prev); // negation will be printed in the next move
-    };
-  return(
+        return square;
+      });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
+    });
+    setCurrentMove(prev => prev + 1); // negation will be printed in the next move
+  };
+
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
+  return (
     <div className="app">
       <h1> TIC TAC TOE </h1>
-      <h2>  {message} </h2>
-      <Board board={board} handleSquareClick={handleSquareClick}/>
+      <h2> {message} </h2>
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
-  )
-}
+  );
+};
 export default App;
--- tictactoe/src/components/Board.js
+++ tictactoe/src/components/Board.js
@@ -1,37 +1,36 @@
 import React, { useState } from 'react';
 import Square from './Square';
 
-const  Board = ({board,handleSquareClick}) => {
+const Board = ({ board, handleSquareClick }) => {
+  const renderSqaure = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
+  return (
+    <div className="board">
+      <div className="board-row">
+        {renderSqaure(0)}
+        {renderSqaure(1)}
+        {renderSqaure(2)}
+      </div>
 
-    
-    const renderSqaure = (position) => {
-        return (<Square value={board[position]}
-        onClick = {() => handleSquareClick(position)}
-        />
-        );
-    };
-     return(
-        <div className="board">
+      <div className="board-row">
+        {renderSqaure(3)}
+        {renderSqaure(4)}
+        {renderSqaure(5)}
+      </div>
 
-            <div className="board-row">
-                { renderSqaure(0)}
-                { renderSqaure(1)}
-                { renderSqaure(2)}
-            </div>
-
-            <div className="board-row">
-                { renderSqaure(3)}
-                { renderSqaure(4)}
-                { renderSqaure(5)}    
-            </div>
-
-            <div className="board-row"> 
-                { renderSqaure(6)}
-                { renderSqaure(7)}
-                { renderSqaure(8)}
-            </div>
-        </div>
-    )
-}
+      <div className="board-row">
+        {renderSqaure(6)}
+        {renderSqaure(7)}
+        {renderSqaure(8)}
+      </div>
+    </div>
+  );
+};
 
 export default Board;

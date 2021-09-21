2f0a7ae2df4904965814baf7b291afe50c6ed154
jasmeen7202 <jasmeen7202@gmail.com>
2021-09-22 01:36:42 +0530

    displayed show results

--- box-office/src/pages/Home.js
+++ box-office/src/pages/Home.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onInputChange = ev => {
     setInput(ev.target.value);
@@ -11,20 +13,34 @@ const Home = () => {
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=men ---------> api link
 
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+      // console.log(result);
+    });
   };
 
-  const onKeyDown = (ev) => {
+  const onKeyDown = ev => {
     if (ev.keycode === 13) {
       onSearch();
     }
     // console.log(ev.keycode);
   };
 
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div> // showNAme is mapped with its unique key value
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
   return (
     <MainPageLayout>
       <input
@@ -36,6 +52,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };

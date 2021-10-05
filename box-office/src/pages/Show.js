31f7e9900091a6ecce21179e891307384126c262
jasmeen7202 <jasmeen7202@gmail.com>
2021-10-05 22:18:57 +0530

    created two new more hooks

--- box-office/src/pages/Show.js
+++ box-office/src/pages/Show.js
@@ -1,65 +1,17 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { apiGet } from '../misc/config';
 import ShowMainData from '../components/show/ShowMainData';
 import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import Cast from '../components/show/Cast';
 import { InfoBlock, ShowPageWrapper } from './Show.styled';
+import { useShow } from '../misc/custom-hooks';
 
-const reducer = (prevState, action) => {
-  switch (action.type) {
-    case 'FETCH_SUCCESS': {
-      return { isLoading: false, error: null, show: action.show };
-    }
-
-    case 'FETCH_FAILED': {
-      return { ...prevState, isLoading: false, err: action.error };
-    }
-    default:
-      return prevState;
-  }
-};
-const initialState = {
-  show: null,
-  isLoading: true,
-  error: null,
-};
 const Show = () => {
   const { id } = useParams();
 
-  const [{ show, isLoading, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-
-  // const [show, setShow] = useState(null);
-  // console.log('params', params);
-
-  // const [isLoading, SetIsLoading] = useState(true);
-  // const [error, setError] = useState(null);
-
-  useEffect(() => {
-    let isMounted = true;
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then(results => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_SUCCESS', show: results });
-        }
-      })
-      .catch(err => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_FAILED', error: err.message });
-        }
-      });
-
-    return () => {
-      isMounted = false;
-    };
-  }, [id]); // whenever we will i/p a value into input field, use effect run will be consoled....
-
-  // console.log('show', show);
+  const { show, isLoading, error } = useShow(id);
 
   if (isLoading) {
     return <div>Data is being loaded</div>;

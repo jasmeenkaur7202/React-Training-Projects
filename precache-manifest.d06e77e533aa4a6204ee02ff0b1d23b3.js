a56f2621ee968294143a6472fd5855a9277ef547
jasmeen7202 <jasmeen7202@gmail.com>
2021-10-06 00:26:47 +0530

    Updates

---
+++ precache-manifest.d06e77e533aa4a6204ee02ff0b1d23b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7093c5c095e5f39e35f8fd244c29c6",
+    "url": "/React-Training-Projects/index.html"
+  },
+  {
+    "revision": "e23347d985f7d3c6a177",
+    "url": "/React-Training-Projects/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "218996e49f5d5cff0c7d",
+    "url": "/React-Training-Projects/static/js/2.357113fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Training-Projects/static/js/2.357113fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23347d985f7d3c6a177",
+    "url": "/React-Training-Projects/static/js/main.a9797e14.chunk.js"
+  },
+  {
+    "revision": "edbf26766ad0eb8ca7d7",
+    "url": "/React-Training-Projects/static/js/runtime-main.d4b42ad8.js"
+  }
+]);

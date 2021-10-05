c5056f25ec5a98369692e57fe358ba55ceb9e046
jasmeen7202 <jasmeen7202@gmail.com>
2021-10-05 16:09:51 +0530

    replaced usestate with usereducer on show page

--- box-office/src/pages/Show.js
+++ box-office/src/pages/Show.js
@@ -1,29 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, err: action.error };
+    }
+    default:
+      return prevState;
+  }
+};
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
 const Show = () => {
   const { id } = useParams();
 
-  const [show, setShow] = useState(null);
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  // const [show, setShow] = useState(null);
   // console.log('params', params);
 
-  const [isLoading, SetIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  // const [isLoading, SetIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
   useEffect(() => {
     let isMounted = true;
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
-          if(isMounted){
-            setShow(results);
-            SetIsLoading(false);
-          }
+        if (isMounted) {
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
+        }
       })
       .catch(err => {
-        if(isMounted){
-        setError(err.message);
-        SetIsLoading(false);
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
         }
       });
 
